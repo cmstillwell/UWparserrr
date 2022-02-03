d634e8c32a58846ab12d018495b7f9b784530f56
Stillwell CM <c.m.stillwell@gmail.com>
2022-02-02 19:55:33 -0600

    Updated function; ready for testing

--- R/norm_date_on_year.R
+++ R/norm_date_on_year.R
@@ -6,31 +6,43 @@
 #' @param format what the function should return, either a date or count of days from the anchor date
 
 norm_date_on_year <- function(date,
-                              acad_year,
-                              anchor_date = as.Date("2020-01-01"),
+                              acad_year = format(Sys.Date(), "%Y") |> as.numeric(),
+                              anchor_date = "2020-01-01",
                               format = "date") {
 
-  is.date <- function(x) {inherits(x, "Date")}
-
-  assertthat::assert_that(is.numeric.Date(date))
-  assertthat::assert_that(assertthat::is.count(acad_year))
-  assertthat::assert_that(is.numeric.Date(anchor_date))
-
-  date <- list(
-    full   = as.Date(date),
-    yr     = strptime(date, format = "%m"),
-    mon    = clock::get_month(date),
-    day    = clock::get_day(date),
-    yr_adj = yr - acad_year
+  assertthat::assert_that(!is.na(lubridate::parse_date_time(anchor_date, orders = "ymd")))
+  anchor_date <- lubridate::parse_date_time(anchor_date, orders = "ymd")
+  anchor <- list(
+    date = anchor_date,
+    yr   = format(anchor_date, "%Y") |> as.numeric(),
+    mon  = format(anchor_date, "%m") |> as.numeric(),
+    day  = format(anchor_date, "%d") |> as.numeric(),
   )
 
-  date_norm <- NULL
+  assertthat::assert_that(assertthat::is.count(acad_year))
+
+  date <- list(
+    full   = ,
+    yr     = format(date, "%Y") |> as.numeric(),
+    mon    = format(date, "%m") |> as.numeric(),
+    day    = format(date, "%d") |> as.numeric(),
+    yr_adj = format(date, "%Y") |> as.numeric() - acad_year
+  )
+
+  assertthat::assert_that(grepl("date|count", format))
+  if (format == "date") {
+    date_norm <- ISOdate(anchor$yr + date$yr_adj, date$mon, date$day)
+  } else if (format == "count") {
+    date_norm <- difftime(ISOdate(anchor$yr + date$yr_adj, date$mon, date$day),
+                          anchor_date,
+                          units = "days")
+  }
 
   return(date_norm)
 }
 
 
-test <- c("2020-07-01", "2020-10-31", "2021-01-14", "2021-06-30", "2021-07-12") %>%
+test <- c("2020-07-01", "2020-10-31", "2021-01-14", "2021-06-30", "2021-07-12") |>
   as.Date()
 
 norm_date_on_year(test)
