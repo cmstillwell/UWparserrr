1aa952a1eac4cbfedd9aa61636f5c470d3c98921
Christopher Stillwell <c.m.stillwell@gmail.com>
2022-03-03 13:34:20 -0600

    New trimmed mean function

---
+++ R/mean_trim_by_n.R
@@ -0,0 +1,26 @@
+#' Calculate a trimmed mean, removing n values from each end of the sequence
+#'
+#' @param x A numeric vector
+#' @param n.trim A number
+#'
+#' @return The vector mean with 'n.trim' values removed from each end before caculating
+#' @export
+#'
+mean_trim_by_n <- function(x, n.trim) {
+
+  # ensure both inputs are numeric and that the trim value atomic
+  assertthat::is.number(x)
+  assertthat::is.number(n.trim)
+  assertthat::assert_that(length(n.trim) == 1)
+
+  n.trim <- round(n.trim)
+
+  trim.pct <- (1 / length(x[!is.na(x)])) * n.trim
+
+  trim.mean <- mean(x,
+                    trim = trim.pct,
+                    na.rm = TRUE)
+
+  return(trim.mean)
+
+}
