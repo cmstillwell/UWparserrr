dd09677e061044fa91bcfba90ed65688828e1cb5
Christopher Stillwell <c.m.stillwell@gmail.com>
2022-03-08 12:55:47 -0600

    Split sort and select files by date into two different functions

--- R/sort_files_by_date.R
+++ R/sort_files_by_date.R
@@ -1,49 +1,24 @@
-#' Sort selected files in a directory based on their date attribute
+#' Sort file list based on their date attribute
 #'
-#' @param folder_path A directory path
-#' @param file_pattern Regular expression selecting file list to be sorted
-#' @param n_return How many files should be returned?
+#' @param file_names A character vector of fully expanded file paths (not a list)
+#' @param n_return The number of files to be returned or "all" of them
 #' @param date_sort Which file date property should be used for sorting?
-#' @param skip_temp Should temporary files be skipped? (Almost always "yes")
-#' @param paths_only Return the full, sorted file paths?
+#' @param return_what What should the output format be, a vector of "paths" or a "table" of files and their attributes?
 #'
-#' @return A list of fully expanded file paths
+#' @return A list of fully expanded file paths or a table of sorts files names with their attributes
 #' @export
 #'
-sort_files_by_date <- function(folder_path  = getwd(),
-                               file_pattern = NULL,
-                               n_return     = NULL,
+sort_files_by_date <- function(file_names,
+                               n_return     = "all",
                                date_sort    = "mtime",
-                               skip_temp    = TRUE,
-                               paths_only   = TRUE){
+                               return_what  = "paths"){
+
   require(tidyverse)
 
   # Error handling
   # Date sort argument correct
   if (!date_sort %>% str_detect('^(m|a|c)time$')) {
     stop('Argument `date_sort` must be one of "mtime", "atime", or "ctime".')
-  }
-  # File name pattern is a length-1 string
-  if (!is.character(file_pattern) | length(file_pattern) != 1) {
-    stop('Argument `file_pattern` must be a string vector of length = 1.')
-  }
-
-  # Get file names
-  file_names <- folder_path %>%
-    list.files(pattern = file_pattern, full.names = TRUE)
-
-  # Strip out meta/temporary files created when a file is open elsewhere
-  if (skip_temp == TRUE) {
-    keep <- str_detect(file_names, "~\\$", negate = TRUE)
-    file_names <- subset(file_names, keep)
-  }
-
-  # Warning - File list length = 0
-  if (length(file_names) == 0) {
-    warning(paste0('Function `list.files` returned no files matching the pattern. ',
-                   'Check both the file search (regex) string: "', file_pattern,
-                   '" and the searched folder: "', folder_path, '".'))
-    return(file_names)
   }
 
   # Sort file names by date passed from function call
