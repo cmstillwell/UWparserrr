8d89c768dec0d61d3a2d8531582418e72418efe8
Christopher Stillwell <c.m.stillwell@gmail.com>
2023-04-20 11:35:56 -0500

    v.1.0 - Bugs fixed; initial testing complete

--- R/delete_old_files.R
+++ R/delete_old_files.R
@@ -7,32 +7,53 @@
 #' @return Defaults to the working directory which helps with piped operations
 #' @export
 #'
-delete_old_files <- function(path, file_pattern, return_dir = TRUE) {
+delete_old_files <- function(dir, file_pattern, return_dir = TRUE) {
 
   # Require the 'fs' package
   require(fs)
 
   # Get file list matching the given regular expression
-  files <- dir_ls(directory, type = "file", glob = file_pattern)
+  files <- dir_ls(dir, type = "file", regexp = file_pattern)
 
-  # Ensure there are multiple files matchign the regular expression
-  assertthat::assert_that(length(files) > 1,
-                          "There must be multiple files matching the pattern in the directory.")
+  # Error handling
+    ## Ensure that 'dir' is a directory
+      assertthat::assert_that(is_dir(dir))
 
-  # Get file info
-  files_info <- file_info(files)
+    ## Ensure there are multiple files matching the regular expression
+    ## Warn and continue (useful for piped expressions)
+    if (length(files) < 2) {
+      if (return_dir == TRUE) {
+        warning(stringr::str_glue('Warning: Fewer than two files matching the pattern "{file_pattern}" were selected.',
+                                  'No old file(s) to delete.',
+                                  'Execution continuing.', .sep = " "))
+        return(dir)
+        exit()
+      }
+      # Stop out with error
+      if (return_dir == FALSE) {
+        stop(stringr::str_glue('Error: Fewer than two files matching the pattern "{file_pattern}" were selected.',
+                               'No old file(s) to delete.',
+                               'Function has nothing to return.',
+                               'Execution halted.', .sep = " "))
+      }
+    }
 
-  # Arrange file list by creation date
-  files_info_sorted <- files_info[order(files_info$mtime),]
+  # Main Code: Find and delete old versions of a file
+    ## Get file info
+      files_info <- file_info(files)
 
-  # Create the list of files to delete
-  files_to_delete <- files_info_sorted[-nrow(files_info_sorted), "path"]
+    ## Arrange file list by creation date
+      files_info_sorted <- files_info[order(files_info$modification_time),]
 
-  # Delete the old files
-  file_delete(files_to_delete)
+    ## Create the list of files to delete
+      files_to_delete <- files_info_sorted[-nrow(files_info_sorted), "path"]
+
+    ## Delete the old files
+      as.character(files_to_delete$path) |>
+        file_delete()
 
   # Return the working directory (useful for piped operations)
-  if (return_dir == TRUE) {
-    return(directory)
-  }
+    if (return_dir == TRUE) {
+      return(dir)
+    }
 }
