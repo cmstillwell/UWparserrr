4054ef8869e3d2c0c5c584cd4872dab33c21cf63
Christopher Stillwell <c.m.stillwell@gmail.com>
2023-11-17 15:54:51 -0600

    Changed update_file_repo function to handle files in destination directory

--- R/update_file_repo.R
+++ R/update_file_repo.R
@@ -10,13 +10,44 @@
 
 update_file_repo <- function(files,
                              destination = getwd(),
+                             clear_dest_dir = FALSE,
                              action = "copy",
                              return = "dir",
                              overwrite = FALSE) {
   require(fs)
+  require(stringr)
 
-  # Ensure a valid return type is indicated
+    # Ensure a valid return type is indicated
   assertthat::assert_that(return == "dir" | return == "logical")
+
+  # Manage the destination directory
+  if (clear_dest_dir == FALSE) {
+    n.dest.file <-
+      dir_ls(destination, type = "file") |>
+      length()
+
+    chr.clear.msg <-
+      str_glue("{n.dest.file} file(s) are already in the destination folder:",
+               "'{destination}'. They have not been altered.")
+  }
+
+  if (clear_dest_dir) {
+    dir_ls(destination, type = "file") |>
+    file_delete()
+
+    chr.clear.msg <-
+      str_glue("All files in '{destination}' have been deleted.")
+  }
+
+
+  if (is.character(clear_dest_dir)) {
+    dir_ls(destination, regexp = clear_dest_dir) |>
+    file_delete()
+
+    chr.clear.msg <-
+      str_glue("All files in '{destination}' matching the regular expression ",
+               "'{clear_dest_dir}' have been deleted.")
+    }
 
   # process only if the vector of file names has length > 0
   if (length(files) > 0) {
@@ -29,13 +60,11 @@ update_file_repo <- function(files,
       file_delete(files)
     }
 
-    output_msg = paste0(
-      action,
-      " operation completed. ",
-      length(dest_path),
-      " file(s) processed."
-      )
-
+    output_msg <-
+      str_glue(chr.clear,
+               "{action} operation completed",
+               "{length(dest_path)} new file(s) processed.",
+               .sep = "; ")
     message(output_msg)
 
     # Set return value
@@ -44,7 +73,7 @@ update_file_repo <- function(files,
 
   # return error for when file path argument length equals zero.
   if (length(files) == 0) {
-    warning("No files processed because argument was length 0.")
+    warning(str_glue("No files to {action} because the 'files' argument length is 0."))
 
     return(ifelse(return == "dir", destination, FALSE))
   }
